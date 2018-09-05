a636d76f0409e42e3e7ca4d82b4af4901bcbc9e5
Ben Randerson <ben.m.randerson@gmail.com>
2018-09-06 00:11:26 +0100

    Added basic wall thickness calculation functionality

--- wtgui/__main__.py
+++ wtgui/__main__.py
@@ -1,4 +1,7 @@
 from tkinter import *
+from tkinter.messagebox import showinfo
+
+from wtgui.wallthick import calc_wallthick
 
 TITLE = "WallThick"
 
@@ -15,7 +18,6 @@ class App:
         try:
             self.root.mainloop()
         except KeyboardInterrupt:
-            # self.log.info("Keyboard interrupt called. Shutting down.")
             sys.exit()
 
 
@@ -27,7 +29,7 @@ class RootView:
     def __init__(self, root):
         # Configure root
         self.root = root
-        self.root.minsize(200, 200)
+        # self.root.minsize(200, 200)
         self.root.title(TITLE)
         self.root.rowconfigure(0, weight=1)
         self.root.columnconfigure(0, weight=1)
@@ -61,7 +63,8 @@ class RootView:
         self.root.config(menu=self.root_menu)
 
         self.app_menu = Menu(self.root_menu)
-        self.root_menu.add_cascade(label='App', menu=self.app_menu)
+        self.root_menu.add_cascade(label='WTGUI', menu=self.app_menu)
+        self.app_menu.add_command(label='Quit WTGUI', command=self.frame.quit)
 
         self.file_menu = Menu(self.root_menu)
         self.root_menu.add_cascade(label='File', menu=self.file_menu)
@@ -70,22 +73,31 @@ class RootView:
         """
         Confugure the main view
         """
-        self.label_1 = Label(self.frame, text="Diameter")
-        self.label_2 = Label(self.frame, text="Pressure")
+        self.diameter_label = Label(self.frame, text="Diameter [mm]")
+        self.pressure_label = Label(self.frame, text="Pressure [bar]")
+        self.smys_label = Label(self.frame, text="SMYS [MPa]")
         self.diameter = Entry(self.frame)
         self.pressure = Entry(self.frame)
+        self.smys = Entry(self.frame)
 
-        self.label_1.grid(row=0, sticky=E)
-        self.label_2.grid(row=1, sticky=E)
+        self.diameter_label.grid(row=0, sticky=W)
+        self.pressure_label.grid(row=1, sticky=W)
+        self.smys_label.grid(row=2, sticky=W)
+
         self.diameter.grid(row=0, column=1)
         self.pressure.grid(row=1, column=1)
+        self.smys.grid(row=2, column=1)
 
         self.runButton = Button(self.frame, text="Run",
                                 command=self.runCalculation)
-        self.runButton.grid(row=2, column=1, sticky=E)
+        self.runButton.grid(row=3, column=1, sticky=E)
 
     def runCalculation(self):
-        print("Wall thickness calculation running")
+        d = 0.001 * float(self.diameter.get())
+        p = 100_000 * float(self.pressure.get())
+        smys = 1_000_000 * float(self.smys.get())
+        t = calc_wallthick(d, p, smys)
+        showinfo("Minimum Wall Thickness", f'{1000 * t:.2f} mm')
 
 
 def main():
---
+++ wtgui/wallthick.py
@@ -0,0 +1,2 @@
+def calc_wallthick(d, p, smys):
+    return p * d / 0.72 / smys
